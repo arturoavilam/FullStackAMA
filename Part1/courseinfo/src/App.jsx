82dc3cfe9934e47946367d717bdde3db13a35f8d
Arturo Avila <avilama@outlook.com>
2024-10-15 09:22:22 -0700

    Update App.jsx

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,7 +1,7 @@
 const Header = (props) => {
+  console.log(props)
   return <h1>{props.course}</h1>
 }
-
 
 const Content = (props) => {
   console.log(props)
@@ -25,18 +25,18 @@ const Total = (props) => {
 const App = () => {
   const course = {
     name:"Half Stack application development",
-  }
-  const parts = [
-    { name: "Fundamentals of React", exercises: 10 },
-    { name: "Using props to pass data", exercises: 7},
-    { name: "State of a component", exercises: 14,}
+    parts : [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7},
+      { name: "State of a component", exercises: 14,}
   ]
+  }
 
   return ( 
     <div>
-      <Header course = {course.name} />
-      <Content parts = {parts}/>
-      <Total parts = {parts}/>
+      <Header course = {course.name}/>
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
     </div>
   )
 }
